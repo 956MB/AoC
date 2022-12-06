6fa6dba68257b596644aec08e74ded26377830f3
Alexander Bays <baysicsss@gmail.com>
2022-12-06 02:11:28 -0600

    226
    easy?

---
+++ 22/6/6.py
@@ -0,0 +1,15 @@
+import time
+import re
+
+# https://adventofcode.com/2022/day/6
+
+def _1(inp, mlen):
+    for i in range(0, len(inp)-mlen, 1):
+        if len(set(inp[i:i+mlen])) >= mlen: return i+mlen
+
+if __name__ == '__main__':
+    inp = open('p2.txt', 'r').read()
+    start = time.time()
+    print(_1(inp, 14))
+    end = time.time()
+    print(end - start)
